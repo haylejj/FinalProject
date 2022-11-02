142de774373006af67f6e1b5f661f31d07df0b8b
Cagatay Berk Cingiz <95578046+haylejj@users.noreply.github.com>
2022-11-02 13:24:21 +0300

    2 Kasım

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,13 +9,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
+            foreach (var category  in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EFProductDal());
 
             foreach (var item in productManager.GetAllByCatogoryId(2))
             {
                 Console.WriteLine(item.ProductName);
             }
-
         }
     }
 }
